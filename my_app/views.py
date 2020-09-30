1eac27b183abbd4138c7053ce856c76fdc704b9d
Euhid Aman <aman.derax20@gmail.com>
2020-09-30 22:04:01 +0530

    made directory with empty static file 'style.css'

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,8 @@
+# video ==> 7:49:00
+
 from django.shortcuts import render
+
 
 # Create your views here.
 def home(request):
-    return render(request,'base.html')
+    return render(request, 'base.html')
