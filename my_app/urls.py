b92ced541490929a1cc948e59c98818e974869d7
Euhid Aman <aman.derax20@gmail.com>
2020-10-01 21:28:32 +0530

    Created new skeleton search function view, and added it to urls.py

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('new_search/', views.new_search, name="new_search"),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -5,4 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html/')
+    return render(request, 'base.html')
+
+def new_search(request):
+    return render(request, 'my_app/new_search.html')
