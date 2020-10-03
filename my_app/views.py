3a30aae635d7df3698d3e15619a3661eb23e04d6
Euhid Aman <aman.derax20@gmail.com>
2020-10-03 22:56:21 +0530

    extracted image url

--- my_app/views.py
+++ my_app/views.py
@@ -1,12 +1,14 @@
-# video ==> 9:20:00
+# video ==> 9:41:30
 import requests
 from requests.compat import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 from . import models
 
-
 BASE_CRAIGSLIST_URL = "https://bangalore.craigslist.org/search/?query={}"
+BASE_IMAGE_URL = "https://images.craigslist.org/{}_300x300.jpg"
+
+
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
@@ -36,10 +38,20 @@ def new_search(request):
             post_price = post.find(class_='result-price').text
         else:
             post_price = 'N/A'
-        final_postings.append((post_title, post_url, post_price))
 
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = "https://sfwallpaper.com/images/image-not-available-6.jpg"
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+
+    # elements to be sent to frontend, in the form of dictionary
     stuff_for_frontend = {
         'search': search,
         'final_postings': final_postings,
     }
+
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
